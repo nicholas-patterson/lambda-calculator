8b5e115b8515be58fac7eba75f38dac9b51ecd51
Nicholas Patterson <nicholaspatterson@Nicholass-MBP.home>
2019-07-16 15:13:41 -0400

    started project sass is not working

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,22 @@
 import React from "react";
 
 //import any components needed
+import OperatorButton from "./OperatorButton";
 
 //Import your array data to from the provided data file
+import { operators } from "../../../data";
 
-const Operators = () => {
+const Operators = props => {
   // STEP 2 - add the imported data to state
+  const [operator, setOperator] = useState(operators);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {operator.map(op => (
+        <OperatorButton op={op} />
+      ))}
     </div>
   );
 };
